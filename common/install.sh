5391455f4606ffcd699ed8b64ede045e07915b42
NotZeetaa <rodrigo2005contente@gmail.com>
2021-07-02 00:27:03 +0100

    tree: Bring up to Dt2wModule
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- common/install.sh
+++ common/install.sh
@@ -4,33 +4,33 @@ mkdir -p $MODPATH/system/bin
 
 ui_print " "
 ui_print "    ×××××××××××××××××××××××××××××××"
-ui_print "       Select Adb Mode:  📳     "
+ui_print "       Select Dt2w Mode:  📳     "
 ui_print "    ×××××××××××××××××××××××××××××××"
 ui_print " "
 ui_print "  Vol+ = Next; Vol- = Select"
 ui_print " "
-ui_print "  1. Adb Enabler "
-ui_print "  2. Adb Disabler "
+ui_print "  1. Dt2w Enabler "
+ui_print "  2. Dt2w Disabler "
 ui_print " "
 ui_print "  Select: "
-AD=1
+DT=1
 while true; do
-ui_print "   $AD"
+ui_print "   $DT"
 	if $VKSEL; then
-		AD=$((AD + 1))
+		DT=$((DT + 1))
 	else 
 		break
 	fi
-	if [ $AD -gt 2 ]; then
-		AD=1
+	if [ $DT -gt 2 ]; then
+		DT=1
 	fi
 done
 ui_print " "
-ui_print "  Selected: $AD"
+ui_print "  Selected: $DT"
 #
-case $AD in
- 1 ) FCTEXTAD1="Adb Enabler"; cp -af $TMPDIR/mode/adbE $MODPATH/system/bin;;
- 2 ) FCTEXTAD1="Adb Disabler"; cp -af $TMPDIR/mode/adbD $MODPATH/system/bin;;
+case $DT in
+ 1 ) FCTEXTAD1="Dt2w Enabler"; cp -af $TMPDIR/mode/dtE $MODPATH/system/bin;;
+ 2 ) FCTEXTAD1="Dt2w Disabler"; cp -af $TMPDIR/mode/dtD $MODPATH/system/bin;;
 esac
 ui_print " "
 ui_print "- Mode: $FCTEXTAD1 "
